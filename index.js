6d85a210aeb1f6bdac3cb8acbb0d6813bbf7170e
Cameron Tredoux <camtredoux@gmail.com>
2021-10-03 21:28:02 -0600

    Displaying bars after they have been sorted, still need to visualize the process

---
+++ index.js
@@ -0,0 +1,36 @@
+function bubbleSort(items) {
+  for (let i = 0; i < items.length; i++) {
+    for (let j = 0; j < items.length - 1; j++) {
+      if (items[j].offsetHeight > items[j + 1].offsetHeight) {
+        swap(j, j + 1, items)
+      }
+    }
+  }
+  console.log(items);
+}
+
+function swap(a, b, list) {
+  let temp = list[a].offsetHeight
+  list[a].style.height = `${list[b].offsetHeight}px`
+  list[b].style.height = `${temp}px`
+}
+
+let items = document.querySelectorAll("[id^='item-']")
+items.forEach(item => {
+  console.log(item);
+})
+
+function generate(values) {
+  items.forEach(item => {
+    item.style.backgroundColor = "red"
+    let random = Math.random() * 100
+    item.style.width = "10px"
+    item.style.height = `${random}px`
+  })
+}
+
+console.log(items);
+
+generate(100)
+
+bubbleSort(items)
