943b05d12c9a2e6c703dee5a43f9b2ce017ba6c6
Cameron Tredoux <camtredoux@gmail.com>
2021-10-03 23:01:30 -0600

    Added async sleep() function to visualize the items after each swap

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-function bubbleSort(items) {
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+async function bubbleSort(items) {
   for (let i = 0; i < items.length; i++) {
     for (let j = 0; j < items.length - 1; j++) {
       if (items[j].offsetHeight > items[j + 1].offsetHeight) {
         swap(j, j + 1, items)
+        await sleep(200)
       }
     }
   }
