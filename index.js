8e6bde79a7ff1ee5eff12e7e28362bbb9ffe43d0
Cameron Tredoux <camtredoux@gmail.com>
2021-10-07 14:47:21 -0600

    Better rendering of current item, and visualizing the two items being compared by changing their color to purple

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms))
 }
@@ -7,22 +6,30 @@ async function bubbleSort(items) {
   let speed = document.getElementById("speed")
   let sleep_ms = speed.value
   for (let i = 0; i < items.length; i++) {
-    for (let j = 0; j < items.length - 1; j++) {
+    for (let j = 0; j < items.length - i - 1; j++) {
       if (items[j].offsetHeight > items[j + 1].offsetHeight) {
-        swap(j, j + 1, items)
-        await sleep(sleep_ms)
-        items[j + 1].style.backgroundColor = "red"
+        await swap(j, j + 1, items, sleep_ms)
       }
+      render(items, j + 1, "green")
+      render(items, j, "red")
+      await sleep(sleep_ms)
     }
     items[items.length - i - 1].style.backgroundColor = "blue"
   }
 }
 
-function swap(a, b, list) {
+function render(items, i, color) {
+  items[i].style.backgroundColor = color
+}
+
+async function swap(a, b, list, sleep_ms) {
+  render(list, a, "purple")
+  render(list, b, "purple")
+  await sleep(sleep_ms)
   let temp = list[a].offsetHeight
   list[a].style.height = `${list[b].offsetHeight}px`
   list[b].style.height = `${temp}px`
-  list[b].style.backgroundColor = "green"
+  await sleep(sleep_ms)
 }
 
 let itemsId = document.getElementById("items")
@@ -42,7 +49,7 @@ genBtn.addEventListener("click", () => {
   }
   items = itemsId.querySelectorAll("[id^='item-']")
   items.forEach(item => {
-    let random = Math.random() * 200
+    let random = (Math.random() * 200) + 10
     item.style.height = `${random}px`
   })
 })
